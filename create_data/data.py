a5d0a6c94ec5b2f74bab483c59468d907f576b31
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2023-08-21 23:26:34 +0000

    refactor: ajustando script data.py

--- create_data/data.py
+++ create_data/data.py
@@ -2,84 +2,69 @@ import psycopg2
 import random
 import csv
 
-
 def loadcsv():
     csv_file_path = "ibge-fem-10000.csv"
-
-    # Lists to store data
     data_to_insert = []
 
-    # Read data from CSV
     with open(csv_file_path, "r") as csv_file:
         csv_reader = csv.reader(csv_file)
         next(csv_reader)  # Skip header row
 
         for row in csv_reader:
             data_to_insert.append(row[0].lower())
-    #print(data_to_insert)
     return data_to_insert
 
-
-def create_conection():
-    # Connect to the PostgreSQL database
+def create_connection():
     conn = psycopg2.connect(
         host="localhost",
         database="dojo",
         user="postgres",
-        password=""
+        password="postgres"
     )
     conn.set_session(autocommit=False)
-    # Create a cursor
     cur = conn.cursor()
-    
+
     cur.execute("truncate table empregados;")
     cur.execute("truncate table departamentos;")
-    print("Droping tables...")
-    return conn,cur
+    print("Dropping tables...")
 
+    return conn, cur
 
-
-# Generate and insert N rows of synthetic data
-nomes=loadcsv()
-conn,cur=create_conection()
+nomes = loadcsv()
+conn, cur = create_connection()
 random_values = list(range(1000, 10000, 100))
-
-# Shuffle the list to randomize the order
 random.shuffle(random_values)
 
-#number of tuples
-N=10000000
-#Departaments
-dep_names=["Administrativo", "Atacado", "Atendimento ao cliente", "Auditoria", "Comercial", "Comunicação", "Contabilidade", "Controladoria", "Desenvolvimento", "Estratégia", "Esportes", "Financeiro", "Garantia de Qualidade",  "Inteligência de Mercado", "Legal", "Logística", "Manutenção", "Marketing", "Operações", "Pesquisa e Desenvolvimento", "Planejamento", "Planejamento Financeiro", "Processos", "Produção", "Projetos", "Recursos Humanos", "Seguros", "Tesouraria", "TI Tecnologia da Informação", "Treinamento e Desenvolvimento", "Tributário, Fiscal", "Varejo", "Vendas"]
+N = 10000000
+dep_names = [
+    "Administrativo", "Atacado", "Atendimento ao cliente", "Auditoria", "Comercial",
+    "Comunicação", "Contabilidade", "Controladoria", "Desenvolvimento", "Estratégia",
+    "Esportes", "Financeiro", "Garantia de Qualidade",  "Inteligência de Mercado",
+    "Legal", "Logística", "Manutenção", "Marketing", "Operações", "Pesquisa e Desenvolvimento",
+    "Planejamento", "Planejamento Financeiro", "Processos", "Produção", "Projetos",
+    "Recursos Humanos", "Seguros", "Tesouraria", "TI Tecnologia da Informação",
+    "Treinamento e Desenvolvimento", "Tributário, Fiscal", "Varejo", "Vendas"
+]
 
-#populate empregados table
-print("Populate table empregados wiht " ,N, " tuples")
+print(f"Populating table empregados with {N} tuples")
 for emp_id in range(0, N):
-    dep_id = random.randint(1, len(dep_names))  # Assuming 5 departments
+    dep_id = random.randint(1, len(dep_names))
     supervisor_id = random.randint(1, 10) if emp_id > 1 else 0
-    nome = f"{nomes[random.randint(0, len(nomes)-1)]}"
-    salario =(random_values[random.randint(1, len(random_values)-1)])
-    
+    nome = nomes[random.randint(0, len(nomes) - 1)]
+    salario = random_values[random.randint(0, len(random_values) - 1)]
+
     insert_query = f"INSERT INTO empregados (emp_id, dep_id, supervisor_id, nome, salario) VALUES ({emp_id}, {dep_id}, {supervisor_id}, '{nome}', {salario});"
     cur.execute(insert_query)
 
-
-
-#populate departamentos table
-print("Populate table dep_names...")
-for dep in range(0,len(dep_names)):
-    dep_id=dep
-    nome=f"{dep_names[random.randint(0, len(dep_names)-1)]}"    
+print("Populating table departamentos...")
+for dep_id, nome in enumerate(dep_names):
     insert_query = f"INSERT INTO departamentos VALUES ({dep_id}, '{nome}');"
     cur.execute(insert_query)
 
-# Commit the changes and close the connection
 conn.commit()
 
-cur = conn.cursor()
-cur.execute("select count(*) from empregados;")
-print("Number of tuples in the table emp is ",cur.fetchone()[0])
-
+cur.execute("SELECT count(*) FROM empregados;")
+print("Number of tuples in the table empregados:", cur.fetchone()[0])
 
 cur.close()
 conn.close()
